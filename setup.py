4fd4737d658209b2f303945eaeb13f3188bbf191
washim <washim.ahmed@gmail.com>
2018-09-08 08:03:19 +0000

    0.0.3 realeased

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='pyajax',
-    version='0.0.2',
+    version='0.0.3',
     url='https://github.com/washim/pyajax',
     license='BSD',
     author='Washim Ahmed',
