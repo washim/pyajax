d2b618dbdbdde6b49d1d084713d60c5ab1b4504d
washim <washim.ahmed@gmail.com>
2018-09-08 08:22:55 +0000

    0.0.4 released

--- pyajax/__init__.py
+++ pyajax/__init__.py
@@ -1,1 +1,25 @@
 name = "pyajax"
+
+def buildscript(config):
+    script  = "<script type='text/javascript'>"
+    script += "$(function() {"
+
+    for callback in config:
+        uinputs = list(("'%s': $('#%s').val()" % (key, val)) for key, val in callback['handler']['input'].items())
+        actions = list(("$('#%s').html(response.%s);" % (key, val)) for key, val in callback['handler']['action'].items())
+
+        script += """
+        $('#%s').bind('%s', function() {
+            $.getJSON('%s', %s).done(function(response){%s});
+            return false;
+        });
+        """ % (callback['handler']['event']['target'], 
+               callback['handler']['event']['method'], 
+               callback['handler']['event']['url'],
+               "{"+",".join(uinputs)+"}",
+               "".join(actions))
+
+    script += "});" 
+    script += "</script>" 
+
+    return script
